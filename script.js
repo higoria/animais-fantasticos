bc45ebce8d6582866fd4558c3cd555685ee0bb14
Portifolio Pessoal <higormadara1@gmail.com>
2024-01-22 17:31:24 -0300

    criaçao de varias coisas ai

--- script.js
+++ script.js
@@ -1,36 +1,78 @@
-// Verifique a distância da primeira imagem
-// em relação ao topo da página
-const primeiraImg = document.querySelector("img");
-console.log(primeiraImg.offsetTop);
+// CODIGO DE GALERIA DE IMGS E TEXTO
 
-// Retorne a soma da largura de todas as imagens
-const todasImgs = document.querySelectorAll("img");
-todasImgs.forEach((item) => {
-  let resultadoSoma = 0;
-  resultadoSoma = resultadoSoma + item.offsetWidth;
-  console.log(resultadoSoma);
-});
+const tabMenu = document.querySelectorAll(".js-tabmenu li");
+const tabConteudo = document.querySelectorAll(".js-tabconteudo section");
 
-// Verifique se os links da página possuem
-// o mínimo recomendado para telas utilizadas
-// com o dedo. (48px/48px de acordo com o google)
-const linksPagina = document.querySelectorAll("a");
-
-linksPagina.forEach((link) => {
-  link.getBoundingClientRect();
-  link.width;
-  link.Height;
-  console.log(link.width, link.Height);
-});
-
-// Se o browser for menor que 720px,
-// adicione a classe menu-mobile ao menu
-const tamanhoPagina = window.innerWidth;
-console.log(tamanhoPagina);
-if (tamanhoPagina < 720) {
-  const menu = document.querySelector(".nav-conteiner");
-  menu.classList.add("menu-mobile");
-  console.log("dispositivo Mobile");
-} else {
-  console.log("Dispositivo Desktop");
+function activeTab(index) {
+  tabConteudo.forEach((section) => {
+    section.classList.remove("ativo");
+  });
+  tabConteudo[index].classList.add("ativo");
 }
+
+tabMenu.forEach((itemsLI, index) => {
+  itemsLI.addEventListener("click", () => {
+    activeTab(index);
+  });
+});
+// FIM CODIGO DE GALERIA DE IMGS E TEXTO
+
+// CODIGO DAS PERGUNTAS
+const perguntasFAQ = document.querySelectorAll(".js-accordion dt");
+perguntasFAQ[0].classList.add("ativoDD");
+const DDperguntas = document.querySelectorAll(".js-accordion dd");
+perguntasFAQ[0].nextElementSibling.classList.add("ativoDD");
+
+function acionarPergunta() {
+  this.nextElementSibling.classList.toggle("ativoDD");
+}
+
+perguntasFAQ.forEach((perguntas) => {
+  perguntas.addEventListener("click", acionarPergunta);
+});
+// fim CODIGO DAS PERGUNTAS
+
+// SCROLL SUAVE
+const linksInternos = document.querySelectorAll('.nav-conteiner a[href^="#"]');
+
+function scrollSuave(event) {
+  event.preventDefault();
+  const href = event.currentTarget.getAttribute("href");
+  const section = document.querySelector(href);
+
+  section.scrollIntoView();
+  section.scrollIntoView({ block: "start", behavior: "smooth" });
+}
+
+linksInternos.forEach((links) => {
+  links.addEventListener("click", scrollSuave);
+});
+// FIM SCROLL SUAVE
+
+// SCROLL 2
+const sectionsDoSite = document.querySelectorAll(".js-scroll");
+const windowMetade = window.innerHeight * 0.4;
+
+function animationScroll() {
+  sectionsDoSite.forEach((sections) => {
+    const topoDasSections = sections.getBoundingClientRect().top - windowMetade;
+    if (topoDasSections < 0) {
+      sections.classList.add("ativoScroll");
+    }
+  });
+}
+animationScroll();
+window.addEventListener("scroll", animationScroll);
+// FIM SCROLL 2
+
+// BOTAO TEMA ESCURO
+const botaoTemaEscuro = document.querySelector(".botaoTemaEscuro");
+
+function acionarTemaEscuro() {
+  document.body.classList.add("temaescuro");
+  document.body.classList.toggle("temaclaro");
+  if (document.body.classList.contains("temaclaro"));
+}
+
+botaoTemaEscuro.addEventListener("click", acionarTemaEscuro);
+// FIM BOTAO TEMA ESCURO
